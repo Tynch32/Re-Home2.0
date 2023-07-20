db2945d57b5b4ec6feb091f72d5a4c4a2dc28598
brayan <carusibrian@gmail.com>
2023-07-20 12:20:05 -0300

    eric nos saco de la sala (?

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ app.use(express.static(path.join(__dirname,'public')));
 
 /* Rutas */
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
-app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'views','404.html')));
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
+
+/* PARTIALS */
+app.get("/footer", (req, res) =>  res.sendFile(path.join(__dirname, "views","partials","footer.html")));
+
+
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
