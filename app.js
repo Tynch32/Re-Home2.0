ed9c3848b373fe389b22a21733876345b9d0b684
Martin <velazquezmartingonzalo@gmail.com>
2023-07-28 12:31:13 -0300

    se añadieron las rutas al app a las demas paginas faltantes

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.use(express.static(path.join(__dirname,'public')));
 
 /* Rutas */
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/productDetail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
+app.get('/productCart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'views','404.html')));
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
