0b2656579c19909684d0c01fc9ed3d331f73a4c1
Martin <velazquezmartingonzalo@gmail.com>
2023-07-31 11:58:47 -0300

    Se reorganizaron los archivos en carpetas, re corrigieron las rutas en app, y se agregaron header y footer a las paginas

--- app.js
+++ app.js
@@ -10,17 +10,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get('/productcart', (req, res) => res.sendFile(path.join(__dirname, 'views', 'producCart.html')));
 app.get('/productDetail', (req, res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
-//app.get('/productCart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
-//app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
-//app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/header', (req, res) => res.sendFile(path.join(__dirname, 'views', 'partials', 'header.html')));
+app.get('/footer', (req, res) => res.sendFile(path.join(__dirname, 'views', 'partials', 'footer.html')));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'views', '404.html')));
 
 
-/* PARTIALS */
-app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "partials", "register.html")));
-app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "partials", "login.html")));
-app.get("/footer", (req, res) => res.sendFile(path.join(__dirname, "views", "partials", "footer.html")));
 
 
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
