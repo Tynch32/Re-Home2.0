80e96ad6641fd41a4649ec7615404d96a2f201c8
MarisaFerrada <ferradamarisa@gmail.com>
2023-08-13 21:34:53 -0300

    hice la vista de edicion de producto y su formulario, agregue las categorias,agregueel archivo scrip para usar boostrap

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,11 +3,11 @@ const userController = require('../controllers/userController');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/productCart', userController.register);
-router.get('/productDetail', userController.login);
+router.get('/productCart', userController.productCart);
+router.get('/productDetail', userController.productDetail);
 router.get('/productAdd', userController.productAdd);
 router.get('/productEdit', userController.productEdit);
-router.get('/register', userController.login);
+router.get('/register', userController.register);
 router.get('/login', userController.login);
 router.get('/*', userController.notFound);
 
