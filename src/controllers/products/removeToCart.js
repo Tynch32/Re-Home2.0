9cc0de021b54baf2210312c659be95016fb5b98e
Martin <velazquezmartingonzalo@gmail.com>
2023-10-07 21:01:24 -0300

    remove de carrito de compras funcionando

--- src/controllers/products/removeToCart.js
+++ src/controllers/products/removeToCart.js
@@ -2,25 +2,20 @@ const { readJSON, writeJSON } = require("../../data");
 
 module.exports = (req, res) => {
   const users = readJSON("users.json");
-  const products = readJSON("products.json");
   var user = null;
-  var usersModify= null;
+  var usersModify=null;
+  var productToRemove = req.params.id;
   if (req.session.userLogin) {
     user = users.find((user) => user.id === req.session.userLogin.id);
   }
   const product = req.params.id;
   if (user) {
-    let productToAdd = products.find((element) => element.id == product);
-    if (productToAdd) {
-      if (!user.shoppingCart.find((element) => element.id == productToAdd.id)) {
-        usersModify = users.map(element=>{
-            if(element.id==req.session.userLogin.id){
-                element.shoppingCart.filter(product=> product!=productToAdd.id);
-            }
-            return element;
-        });
+    usersModify=users.map(element=>{
+      if(element.id==req.session.userLogin.id){
+          element.shoppingCart=element.shoppingCart.filter(product=>product!=productToRemove);
       }
-    }
+      return element;
+  });
     writeJSON(usersModify,'users.json');
     return res.redirect('/'); //VEIRIFICAR POR LAS DUDAS
   } else {
