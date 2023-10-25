c76d40315be937cee887c02b52aa04475d568bbe
Martin <velazquezmartingonzalo@gmail.com>
2023-10-25 13:15:03 -0300

    Se arreglo el menu de seleccion de imagenes, en productDetail

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,6 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
+    
     destination : (req,file,cb) => {
         return cb(null, './public/img/products')
     },
