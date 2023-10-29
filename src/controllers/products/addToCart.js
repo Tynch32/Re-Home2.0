ab006ff62200fd398085b5bb8a4fc612894259ca
Martin <velazquezmartingonzalo@gmail.com>
2023-10-29 14:28:48 -0300

    Carrito de compras completo

--- src/controllers/products/addToCart.js
+++ src/controllers/products/addToCart.js
@@ -1,29 +1,14 @@
-const { readJSON, writeJSON } = require("../../data");
+const db = require("../../database/models");
 
 module.exports = (req, res) => {
-  const users = readJSON("users.json");
-  const products = readJSON("products.json");
-  var user = null;
-  var usersModify=null;
-  if (req.session.userLogin) {
-    user = users.find((user) => user.id === req.session.userLogin.id);
-  }
-  const product = req.params.id;
-  if (user) {
-    let productToAdd = products.find((element) => element.id == product);
-    if (productToAdd) {
-      if (!user.shoppingCart.find((element) => element.id == productToAdd.id)) {
-        usersModify = users.map(element=>{
-            if(element.id==req.session.userLogin.id){
-                element.shoppingCart.push(productToAdd.id);
-            }
-            return element;
-        });
-      }
-    }
-    writeJSON(usersModify,'users.json');
-    return res.redirect('/'); 
-  } else {
-    return res.redirect('/users/login');
-  }
-};
+  db.User.findByPk(req.session.userLogin.id)
+  .then(user=>{
+    db.Order.create({
+      product_id: req.params.id,
+      amount: 1,
+      shoppingcart_id:user.shoppingcart_id
+    }).then(()=> {return res.redirect('/')})
+    .catch(error=>console.log(error));
+  }).catch(error=>console.log(error));
+  
+}
--- src/controllers/products/removeToCart.js
+++ src/controllers/products/removeToCart.js
@@ -1,24 +1,9 @@
-const { readJSON, writeJSON } = require("../../data");
+const db = require("../../database/models");
 
 module.exports = (req, res) => {
-  const users = readJSON("users.json");
-  var user = null;
-  var usersModify=null;
-  var productToRemove = req.params.id;
-  if (req.session.userLogin) {
-    user = users.find((user) => user.id === req.session.userLogin.id);
-  }
-  const product = req.params.id;
-  if (user) {
-    usersModify=users.map(element=>{
-      if(element.id==req.session.userLogin.id){
-          element.shoppingCart=element.shoppingCart.filter(product=>product!=productToRemove);
-      }
-      return element;
-  });
-    writeJSON(usersModify,'users.json');
-    return res.redirect('/'); 
-  } else {
-    return res.redirect('/users/login');
-  }
+  db.Order.destroy({ where: {
+    product_id: req.params.id}})
+  .then(()=>{
+    return res.redirect('/')
+  }).catch(error=>console.log(error));
 };
