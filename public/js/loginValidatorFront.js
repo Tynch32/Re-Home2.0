93e21cb01ae903d66bbadad0133c56bed20b6dec
Martin <velazquezmartingonzalo@gmail.com>
2023-11-03 13:00:16 -0300

    Validaciones Front login

---
+++ public/js/loginValidatorFront.js
@@ -0,0 +1,25 @@
+let $ = id => document.getElementById(id)
+let validarEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+    $('email').addEventListener('blur',function(){
+        if(!validarEmail.test(this.value)){
+            if(!this.value.trim()){
+                $('login_form_email').innerText="* El email es obligatorio";
+                $('login_form_email').hidden = false
+            }else{
+                $('login_form_email').innerText="* No es un email valido";
+                $('login_form_email').hidden = false
+            }
+        }else{
+            $('login_form_email').hidden = true
+        }
+    }) 
+
+    $('password').addEventListener('blur',function(){
+        if(!this.value.trim()){
+            $('login_form_password').hidden = false
+        }else{
+            $('login_form_password').hidden = true
+        }
+    })
