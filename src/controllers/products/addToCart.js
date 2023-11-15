f27bd0f93eca9b115f75643e771fa4547468b477
Martin <velazquezmartingonzalo@gmail.com>
2023-11-15 13:27:30 -0300

    Se arreglo la vista carrito, ahora suma items

--- src/controllers/products/addToCart.js
+++ src/controllers/products/addToCart.js
@@ -1,8 +1,13 @@
 const db = require("../../database/models");
 
-module.exports = (req, res) => {
-  db.User.findByPk(req.params.id)
-  .then(user=>{
+module.exports = async (req, res) => {
+  const product = await db.Order.findOne({
+    where:{
+      product_id:req.params.id,
+      shoppingcart_id:req.body.var1
+    }
+  })
+  if(!product){
     db.Order.create({
       product_id: req.params.id,
       amount: 1,
@@ -10,6 +15,11 @@ module.exports = (req, res) => {
     }).then(()=> 
       {return res.redirect('/')})
     .catch(error=>console.log(error));
-  }).catch(error=>console.log(error));
+  }else{
+    product.amount+=1;
+    await product.save()
+    return res.redirect('/')
+  }
+  
   
 }
--- src/controllers/users/productCart.js
+++ src/controllers/users/productCart.js
@@ -21,7 +21,8 @@ module.exports = async (req,res) => {
     }).then(async carrito=>{
         let total=0;
         await carrito.forEach(producto => {
-            total+= producto.product.price - (producto.product.price * producto.product.discount / 100);
+            total+= ((producto.product.price - (producto.product.price * producto.product.discount / 100))*producto.amount);
+            console.log(total);
         });
         return res.render('productCart',{carrito,total,addPuntos})
     })
