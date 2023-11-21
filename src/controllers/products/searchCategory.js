0801014cd732ff22a8c7b659ec0841228cb64b9b
Martin <velazquezmartingonzalo@gmail.com>
2023-11-20 23:24:03 -0300

    Se agrego el boton categorias, cuando se busca x categorias

--- src/controllers/products/searchCategory.js
+++ src/controllers/products/searchCategory.js
@@ -1,14 +1,16 @@
 const db = require("../../database/models");
 const addPuntos = require('../../middlewares/addPuntos')
 
-module.exports = (req, res) => {
-  
+module.exports = async (req, res) => {
+  let categories = await db.Category.findAll({
+    order:[['name', 'ASC']]
+  })
   db.Product.findAll({where:{
     category_id : req.params.id
   },include:['product_image','product_category']})
     .then((products) => {
         return res.render('searchCategory',{
-          products, id : products[0].product_category.name,addPuntos
+          products, id : products[0].product_category.name,addPuntos,categories
         })
     }).catch((errors) => console.log(errors));
 
