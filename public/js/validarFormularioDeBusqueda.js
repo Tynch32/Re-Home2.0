eacb14c3c396831f8d6630627280816118b8d576
Martin <velazquezmartingonzalo@gmail.com>
2023-12-06 16:56:39 -0300

    no se puede enviar formulario vacio

--- public/js/validarFormularioDeBusqueda.js
+++ public/js/validarFormularioDeBusqueda.js
@@ -1,7 +1,14 @@
-function validarFormulario() {
-    var keywords = document.querySelector('.search-form_input').value.trim();
-    if (keywords == '') {
-        return true;
+const formulariosBusqueda = document.querySelectorAll('.search-form');
+
+formulariosBusqueda.forEach(formulario => {
+  formulario.addEventListener('submit', function(event) {
+
+    const inputBusqueda = formulario.querySelector('input[name="keywords"]');
+
+    if (inputBusqueda.value.trim() === '') {
+
+      event.preventDefault();
+      console.log('No se permitió la búsqueda porque el campo está vacío.');
     }
-    return false;
-}
+  });
+});
