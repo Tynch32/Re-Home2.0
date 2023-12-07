afb1625c0b6b2382676521de1024c71e97b7c493
Martin <velazquezmartingonzalo@gmail.com>
2023-12-06 23:34:13 -0300

    Agregar varios productos al carro

---
+++ public/js/cantButtons.js
@@ -0,0 +1,19 @@
+let botonIzq = document.getElementById("cantProduct_button_left");
+let botonDer = document.getElementById("cantProduct_button_right");
+
+let valor = document.getElementById("product_detail_cant_products");
+let valorActual = parseInt(valor.value);
+
+botonIzq.addEventListener("click",function(event){
+    event.preventDefault();
+    if(valorActual>1){
+        valorActual-=1;
+        valor.value=valorActual;
+    }
+})
+botonDer.addEventListener("click",function(event){
+    event.preventDefault();
+    valorActual+=1;
+    valor.value=valorActual;
+})
+
--- src/controllers/products/addToCart.js
+++ src/controllers/products/addToCart.js
@@ -10,10 +10,10 @@ module.exports = async (req, res) => {
   if(!product){
     db.Order.create({
       product_id: req.params.id,
-      amount: 1,
+      amount: req.body.cantProducts || 1,
       user_id: req.session.userLogin.shoppingcartId
     }).then(()=> {
-      req.session.userLogin.cantItems+=1;
+      req.session.userLogin.cantItems+= +req.body.cantProducts;
       res.cookie('grupoReHome10',req.session.userLogin,{
       maxAge : 1000 * 60 * 60 * 24* 365})
         return res.redirect('/')
@@ -22,7 +22,7 @@ module.exports = async (req, res) => {
   }else{
     product.amount+=1;
     await product.save();
-    req.session.userLogin.cantItems+=1;
+    req.session.userLogin.cantItems+= +req.body.cantProducts;
       res.cookie('grupoReHome10',req.session.userLogin,{
       maxAge : 1000 * 60 * 60 * 24 *365})
     return res.redirect('/');
