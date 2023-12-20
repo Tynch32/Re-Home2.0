ed5352c2abd8ebd779f6b8b520de1da5d6da636b
Martin <velazquezmartingonzalo@gmail.com>
2023-12-20 19:54:16 -0300

    oferts paginator fix

--- src/controllers/products/oferts.js
+++ src/controllers/products/oferts.js
@@ -14,7 +14,7 @@ module.exports = async (req,res) => {
                 },
                 order: [['category_id', 'ASC']],
                 include: ['product_image'],
-                limit: 8,
+                limit: 10,
                 offset: req.skip,
             }),
             db.Product.count({where: {
@@ -25,7 +25,7 @@ module.exports = async (req,res) => {
         ]);
         console.log(itemCount);
         const categories = await db.Category.findAll();
-        const pageCount = Math.ceil(itemCount / 8);
+        const pageCount = Math.ceil(itemCount / 10);
         let cookie = req.cookies.grupoReHome10_cookie;
 
         res.render("index", {
