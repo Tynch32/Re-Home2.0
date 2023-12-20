0a811b14a44b53ea1b93f9c0a2a7dc9c96dc1d5e
Martin <velazquezmartingonzalo@gmail.com>
2023-12-20 16:53:32 -0300

    fix style and components

--- src/controllers/index/index.js
+++ src/controllers/index/index.js
@@ -9,13 +9,13 @@ module.exports = async (req, res) => {
             db.Product.findAll({
                 order: [['category_id', 'ASC']],
                 include: ['product_image'],
-                limit: 8,
+                limit: 10,
                 offset: req.skip,
             }),
             db.Product.count(),
         ]);
         const categories = await db.Category.findAll();
-        const pageCount = Math.ceil(itemCount / 8);
+        const pageCount = Math.ceil(itemCount / 10);
         let cookie = req.cookies.grupoReHome10_cookie;
 
         res.render("index", {
